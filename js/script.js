4eea23847d60eaa7b4f805d2c031ce64f82b9e8a
Leonardo Campan <leonardo.campan@icloud.com>
2021-11-09 21:34:30 +0100

    09.11.2021

--- js/script.js
+++ js/script.js
@@ -1,12 +1,14 @@
-window.onload = function(){displayTable()};
+window.onload = function(){tableNationCity()};
 
-/* creazione tabella generale */
-function displayTable() 
+/* creazione tabella */
+function tableNationCity() 
 {
     s = "<thead>"
         + "<tr>"
         + "<th>" + "NATION" + "</th>" 
         + "<th>" + "CITY" + "</th>" 
+        + "<th>" + "LATITUDE" + "</th>" 
+        + "<th>" + "LONGITUDE" + "</th>" 
         +  "</tr>"
         + "</thead>"  
         + "<tbody>"
@@ -18,6 +20,33 @@ function displayTable()
         s += "<tr>"
         s += '<td class = "nation" id="nation">' + DB[i].SOV0NAME + "</td>"
         s += '<td class = "city">' + DB[i].NAMEASCII + "</td>"
+        s += '<td class = "nation" id="nation">' + DB[i].LATITUDE + "</td>"
+        s += '<td class = "city">' + DB[i].LONGITUDE + "</td>"
+        s += '</tr>'  
+    }
+
+    s += "</tbody>"
+    s += "</table>"
+    document.getElementById("content-table").innerHTML = s;
+}
+
+function tableCityPopulation() 
+{
+    s = "<thead>"
+        + "<tr>"
+        + "<th>" + "CITY" + "</th>" 
+        + "<th>" + "POPULATION" + "</th>" 
+        +  "</tr>"
+        + "</thead>"  
+        + "<tbody>"
+
+    for (var i in DB) 
+    {
+        des = DB[i].NAMEASCII.toUpperCase();
+
+        s += "<tr>"
+        s += '<td class = "nation" id="nation">' + DB[i].NAMEASCII + "</td>"
+        s += '<td class = "city">' + DB[i].POP_MAX + "</td>"
         s += '</tr>'  
     }
 
@@ -27,131 +56,6 @@ function displayTable()
 }
 
 
-
-function countRows() {
-    var totalRows = 0;
-
-    for (let index = 0; index < DB.length; index++) {
-        DB.forEach(element => {
-            
-        });
-        
-    }
-}
-
-
-function ows() {
-    rows = document.getElementById("content-table").rows;
-    console.log(rows);
-}
-
-function displayedRows() {
-    var totalRowCount = 0;
-    var rowCount = 0;
-    var table = document.getElementById("content-table");
-    var rows = table.getElementsByTagName("tr")
-    
-    for (var i = 0; i < rows.length; i++) {
-        totalRowCount++;
-        if (rows[i].getElementsByTagName("td").length > 0) {
-            rowCount++;
-        }
-    }
-
-    var message = "Total Row Count: " + totalRowCount;
-    message += "\nRow Count: " + rowCount;
-    alert(message);
-}
-
-/*
-function sortTable() 
-{
-    const searchInput = document.getElementById("search");
-    const rows = document.querySelectorAll("tbody tr");
-    console.log(rows);
-
-    searchInput.addEventListener("keyup", function (event) 
-    {
-        const q = event.target.value.toLowerCase();
-        rows.forEach((row) => 
-        {
-            row.querySelector("td").textContent.toLowerCase().startsWith(q)
-            ? (row.style.display = "table-row")
-            : (row.style.display = "none");
-        });
-    });
-}
-*/
-
-function init() {
-    // Definisco la variabilie mappa come un oggetto OpenLayers.Map utilizzando il DivMappa, poi aggiungo il Layer OSM (Open Street Map)
-    //var 
-    Mappa = new OpenLayers.Map("DivMappa");
-    //Mappa.addLayer(new OpenLayers.Layer.OSM());
-
-    var mapnik = new OpenLayers.Layer.OSM("OpenStreetMap (Mapnik)");
-    Mappa.addLayer(mapnik);
-
-    // Creo una variabile contenete il layer dei marker poi collego il layer dei markers alla mappa
-    //  var 
-    LayerMarkers = new OpenLayers.Layer.Markers( "Markers" );
-    Mappa.addLayer(LayerMarkers);
-  
-    // Imposto le coordinate di lonLat come centro della mappa di partenza
-    // var 
-    lonLat = new OpenLayers.LonLat(DB.CenterMap_Lon,DB.CenterMap_Lat)
-         .transform(
-           new OpenLayers.Projection("EPSG:4326"), // transform from WGS 1984
-           Mappa.getProjectionObject() // to Spherical Mercator Projection
-         );
-             
-     // imposto centro della mappa e zoom
-     Mappa.setCenter (lonLat, DB.ZoomLevel);
-    } // fine init
-   
-    function visualizza(p) {
-     // posiziono in base ai punti trovati i segnaposto sulla mappa
-     
-     LayerMarkers.clearMarkers();  //#####
-     
-     for (i = 0; i < p.length; i++) {
-     // Creo l'oggetto contenente le coordinate (prima longitudine e poi latitudine)
-     //var
-     LonLat = new OpenLayers.LonLat( p[i].LONGITUDE, p[i].LATITUDE)
-         .transform(
-           new OpenLayers.Projection("EPSG:4326"), // Transformazione da WGS 1984..
-           Mappa.getProjectionObject() // .. a Spherical Mercator Projection
-         );
-     // Aggiungo al layer dei marker un marker (utilizzando l'oggetto lonLat per le coordinate)
-     marker = new OpenLayers.Marker(LonLat);
-     marker.icon.url="./img/default.png";
-     LayerMarkers.addMarker(marker);
-     //LayerMarkers.addMarker(new OpenLayers.Marker(LonLat));
-     } // fine for
-    
-    // alert("prova");
-    // Mappa.removeLayer(LayerMarkers);
-   
-   }  // fine visualizza
-
- 
-   // aggiunta di un Listener alla finestra, il nuovo listener viene usato per la 
-   // gestione parametri: tutti i punti trovati nella pagina 
-   // ricerca.html sono in "event.data"
-   window.addEventListener('message', function(event) {
-   // console.log(event.origin);
-   // IMPORTANT: Check the origin of the data!
-   if (event.origin.indexOf('///')) {
-       // The data has been sent from your site
-       // The data sent with postMessage is stored in event.data
-       visualizza(event.data);
-   //    console.log(event.data[1].NAMEASCII);
-   } else {
-       // The data hasn't been sent from your site!
-       // Be careful! Do not use it.
-       console.log("errore");
-       return;
-   }
   
    }); // fine addEventListener
 
@@ -173,40 +77,78 @@ function init() {
     });
 });
 
-/*
-function displayTable() 
-{
-    s = "<thead>"
-        + "<tr>"
-        + "<th>" + "NATION" + "</th>" 
-        + "<th>" + "CITY" + "</th>" 
-        + "<th>" + "POPULATION" + "</th>" 
-        + "<th>" + "LATITUDE" + "</th>" 
-        + "<th>" + "LONGITUDE" + "</th>"
-        +  "</tr>"
-        + "</thead>"  
-        + "<tbody>"
+var puntitrovati = new Array();
 
-    for (var i in DB) 
-    {
-        des = DB[i].SOV0NAME.toUpperCase();
+function mappaPunti() {
+    var frame = document.getElementById('mappa');
 
-        s += "<tr>"
-        s += '<td class = "nation" id="nation">' + DB[i].SOV0NAME + "</td>"
-        s += '<td class = "city">' + DB[i].NAMEASCII + "</td>"
-        s += '<td class = "population">' + DB[i].POP_MAX +"</td>"
-        s += '<td class = "latitude">' + DB[i].LATITUDE +"</td>"
-        s += '<td class = "longitude">' + DB[i].LONGITUDE +"</td>"
-        s += '</tr>'  
+    frame.contentWindow.postMessage(puntitrovati, '*');
+
+}
+
+function cittav(stato, n) {
+    puntitrovati=new Array();
+    s = "<table>";
+
+    var h = 0;
+    stato = stato.toUpperCase();
+    s += "<tr>";
+        s += "<td id=zero1>" + "" + "</td>";
+            s += "<td id=zero>" + "Stato" + "</td>";
+            s += "<td id=zero>" + "CittÃ " + "</td>";
+            s += "<td id=zero>" + "Popolazione" + "</td>";
+            s += "<td id=zero>" + "Fuso Orario" + "</td>";
+    s += "</tr>";
+    a = 0;
+    risultati = 0;
+    for (var i in DB.Punti) {
+
+        des = DB.Punti[i].SOV0NAME.toUpperCase();
+        
+        if (des.indexOf(stato) >= 0) {
+
+            if (a==0) {
+                s += "<tr>";
+                s += "<td id=uno bgcolor=#a8c0a8 align=center>" + (risultati+1) + "</td>";
+                s += "<td id=due bgcolor=#83aaa8>" + DB.Punti[i].SOV0NAME + "</td>";
+                s += "<td id=tre bgcolor=#83aaa8>" + DB.Punti[i].NAMEASCII + "</td>";
+                s += "<td id=quattro bgcolor=#83aaa8>" + DB.Punti[i].POP_MAX + "</td>";
+                s += "<td id=cinque bgcolor=#83aaa8>" + DB.Punti[i].TIMEZONE + "</td>";
+                s += "</tr>";
+                a = 1;
+            }else{
+                s += "<tr>";
+                s += "<td id=uno bgcolor=#a8c0a8 align=center>" + (risultati+1) + "</td>";
+                s += "<td id=due bgcolor=#b5d0d6>" + DB.Punti[i].SOV0NAME + "</td>";
+                s += "<td id=tre bgcolor=#b5d0d6>" + DB.Punti[i].NAMEASCII + "</td>";
+                s += "<td id=quattro bgcolor=#b5d0d6>" + DB.Punti[i].POP_MAX + "</td>";
+                s += "<td id=cinque bgcolor=#b5d0d6>" + DB.Punti[i].TIMEZONE + "</td>";
+                s += "</tr>";
+                a = 0;
+            }
+            puntitrovati[risultati] = DB.Punti[i];
+            risultati++;
+            
+            
+            h=1;
+            
+        }
     }
 
-    s += "</tbody>"
-    s += "</table>"
-    document.getElementById("content-table").innerHTML = s;
-}
-*/
+    if (h==0) {
+        s= "<tr>";
+        s+= "<td bgcolor=#03ab19 width=4%>" + "La tua ricerca non ha prodotto risultati" + "</td>";
+        s+= "</tr>";
+    }
 
-/****************************************************************************************************************************/
+    s+="</table>";
+
+    mappaPunti();
+    document.getElementById("risultati").innerHTML ="Risultati Trovati: " + risultati;
+    document.getElementById("P1").innerHTML = s;
+    
+}
+
 function capitaliCo(capitali) {
     puntitrovati = new Array();
     s = "<table>";
